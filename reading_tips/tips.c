e5017d7ffcafa34bad6e3e98a59759a075f36f34
Raul Becker <raul.becker@iki.fi>
2017-11-24 02:55:34 +0200

    reading tips deletion works

--- reading_tips/tips.c
+++ reading_tips/tips.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-#define DATAFILE tips.dat
 struct tip {
 	char name[256];
 	char author[256];
@@ -9,16 +8,14 @@ struct tip {
 };
 
 
-
-/* View tips ----------------------------- */
-
 void view_tip() {
 
    struct tip tip;
    FILE *file;
    
    if ( (file=fopen("tips.dat","r")) == NULL) {
-     printf("Error 1\n");
+     printf("No tips\n");
+	 return;
    }
    
    while(1) {
@@ -35,32 +32,40 @@ void view_tip() {
 
 int validate_isbn(char* isbn) {
   if (isbn == NULL) {
+	//printf("got null isbn");
     return 0;
   }
   int digit;
   int digits[18];
   int index = 0;
-  char* p;
-  for (p=isbn; *p; p++) {
-    digit = (int) p;
+  
+  for (char* p=isbn; *p; *p++) {
+    digit = (int) *p;
     if (digit > 47 && digit < 58) {
       digits[index] = digit;
       index = index + 1;
     }
   }
   if (index != 13) {
+	//	printf("not 13 long");
     return 0;
   }
   int total = 0;
-  for (int i = 1; i < 12; i++) {
-    printf("digit: %i\n", digits[i]);
-    total += ((i - 1) % 2 == 0) ? digits[i] * 1 : digits[i] * 3; 
+  int num = 0;
+  for (int i = 1; i <= 12; i++) {
+	num = digits[i-1] - '0';
+	//	int x = digits[i] - '0';
+	//    printf("%i digit: %i\n", i, num);
+    total += ((i+1) % 2 == 0) ? num * 1 : num * 3; 
   }
+  //printf("Total: %i\n", total);
   int chksum = 10 - (total % 10);
+  //printf("cheecksum: %i", chksum);
   if (chksum == 10) {
     chksum = 0;
   }
-  if (digits[12] == chksum) {
+  num = digits[12] - '0';
+  if (num == chksum) {
     return 1;
   } else {
     return 0;
@@ -69,8 +74,6 @@ int validate_isbn(char* isbn) {
 }
 
 
-/* Add tips ----------------------------- */
-
 void add_tip() {
    struct tip tip;
    FILE *file;
@@ -78,27 +81,38 @@ void add_tip() {
    if ( (file=fopen("tips.dat","a")) == NULL) {
      printf("Error 1\n");
    }
+   char name[256];
+   char author[256];
+   char isbn[18];
+   
    
    printf("Name:\n");
-   scanf("%s", &tip.name);
+   scanf("%256s", &name);
 
    printf("Author:\n");
-   scanf("%s", &tip.author);
+   scanf("%256s", &author);
 
    printf("ISBN:\n");
-   scanf("%s", &tip.isbn);
-   if (validate_isbn(tip.isbn)==1) {
+   scanf("%18s", &isbn);
    
-     // while not valid isbn ask for isbn again
-
-     fwrite(&tip,sizeof(tip),1, file);
+   if (validate_isbn(isbn)==1) {
+   
+   } else {
+	 printf("Invalid ISBN, try again:\n");
+	 scanf("%s", &isbn);	 
    }
+   strncpy(&tip.name, &name, 256);
+   strncpy(&tip.author, &author, 256);
+   strncpy(tip.isbn, isbn, 18);
+   //tip.name = name;
+   //tip.author = author;
+   //tip.isbn = isbn;
+   
+   fwrite(&tip,sizeof(tip),1, file);
    
    fclose(file);
    return;
 }
-
-/* Remove tips ----------------------------- */
 
 void remove_tip() {
    struct tip tip;
@@ -120,14 +134,40 @@ void remove_tip() {
 
 	  
    printf("Select the number of the tip you wish to remove:\n");
-   scanf("%d", &choise);
-   
+   scanf("%i", &choise);
    fclose(file);
+   if ( (file=fopen("tips.dat","r")) == NULL) {
+     printf("Error 1\n");
+   }
+
+   FILE *tmpfile;
+   if ( (tmpfile=fopen("tmp.dat","a")) == NULL) {
+     printf("Error 1\n");
+   }
+
+   int cur = 1;
+   while(1) {
+     fread(&tip, sizeof(tip), 1, file);
+     if (feof(file)) {
+       break;
+     }
+	 printf("read block %i with data: %s, %s, %s", cur, tip.name, tip.author, tip.isbn);
+	 if (cur != choise) {
+	     fwrite(&tip,sizeof(tip),1, tmpfile);
+	 }
+	 cur = cur + 1;
+   }
+   fclose(tmpfile);
+   fclose(file);
+    if (remove("tips.dat") == 0) {
+   	 rename("tmp.dat", "tips.dat");
+   	 printf("removed succesfully\n");
+    } else {
+   	 printf("could not remove old file\n");
+   }   
 }
 
 
-
-/* Main program */
 
 
 int main(void) {
